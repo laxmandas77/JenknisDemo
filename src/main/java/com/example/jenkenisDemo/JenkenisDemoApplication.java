5c0a88e74be651e569146a142b40c0b25562e626
laxman <laxmandaspanchireddy9@gmail.com>
2025-08-18 22:58:48 +0530

    initial commit

---
+++ src/main/java/com/example/jenkenisDemo/JenkenisDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.jenkenisDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkenisDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JenkenisDemoApplication.class, args);
+        System.out.println("Welcome to jenkis");
+	}
+
+}
---
+++ src/test/java/com/example/jenkenisDemo/JenkenisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkenisDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkenisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
